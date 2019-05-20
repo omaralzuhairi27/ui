123ced0f7317fb2dd5431b004cce541cc83e62ae
omaralzuhairi27 <omaralzuhairi27@gmail.com>
2019-05-20 15:44:24 +0200

    Upgrade to cloud

--- src/main/java/at/nacs/morseencoderui/logic/Manager.java
+++ src/main/java/at/nacs/morseencoderui/logic/Manager.java
@@ -1,15 +1,13 @@
 package at.nacs.morseencoderui.logic;
 
-import at.nacs.morseencoderui.Message;
 import lombok.RequiredArgsConstructor;
-import org.springframework.beans.factory.annotation.Value;
 import org.springframework.stereotype.Service;
-import org.springframework.web.client.RestTemplate;
 
 @Service
 @RequiredArgsConstructor
 public class Manager {
   private final EncoderUiClient encoderUiClient;
+
   public String post(String message) {
     return encoderUiClient.post(message);
   }
